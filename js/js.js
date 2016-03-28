ec6ad6000bcaaa3b292de9826d0a22caaf89a1d9
Богдан Кожушко < >
2016-03-28 09:22:14 +0300

    Test

--- js/js.js
+++ js/js.js
@@ -4,3 +4,36 @@ $(window).load(function() {
     animation: "slide"
   });
 });
+
+
+$(function loop() {
+
+$("body").animate({temporary_x: 200, temporary_y: 0}, {
+    duration: 7000,
+    step: function() {
+        var position = Math.round(this.temporary_x) + "px " + Math.round(this.temporary_y) + "px";
+        $("body").css("background-position",  position);
+    },
+    complete: function(){
+
+
+$("body").animate({temporary_x: 0, temporary_y: 0}, {
+    duration: 7000,
+    step: function() {
+        var position = Math.round(this.temporary_x) + "px " + Math.round(this.temporary_y) + "px";
+        $("body").css("background-position",  position);
+    },
+    complete: function(){
+
+   loop();
+
+
+
+
+    }
+
+});
+
+    }
+});
+});
